9b3fc27fa52298851e162eba3cac79298c340438
thiago-miranda <thiago.miranda@hotmail.com>
2022-05-08 20:23:02 -0300

    homePage actions button

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:perfect_eggs/blocs/app.bloc.dart';
+import 'package:perfect_eggs/widgets/action-buttons.widget.dart';
 import 'package:perfect_eggs/widgets/type-buttons.widget.dart';
 import 'package:provider/provider.dart';
 
@@ -13,14 +14,15 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          const SizedBox(
-            height: 60,
-          ),
+          const SizedBox(height: 60),
           Text(bloc.selected),
           Text(bloc.state),
           Text(bloc.time.toString()),
           Text(bloc.seconds.toString()),
+          const SizedBox(height: 10),
           const TypeButtons(),
+          const SizedBox(height: 10),
+          const ActionButtons(),
         ],
       ),
     );
